c13cb7ea24da9bace983c1d16da59d4370f75447
Ivanosss <75345481+Ivanosss@users.noreply.github.com>
2021-04-26 17:31:35 -0700

    Update main.py
    merging third branch prep

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ print("party in the main branch")
 
 print("hey boys ivan arrived he brought the margaritas!!!")
 
-print("this is the third branch")
+print("this is the third branch in the second branch")
 
 print("i brought music")
+
+print("lets get the party started")
