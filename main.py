6a923138eaeb05a673ccad1a40c7fbf811f98d06
Juan1541 <73973316+Juan1541@users.noreply.github.com>
2021-04-26 17:55:56 -0700

    Update main.py
    Fixed typos.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-print("this is the fourth branch ")
-print("party in the fourth branch")
+print("This is the fourth branch.")
+print("Party in the fourth branch.")
 
-print("hey boys juan arrived he brought the margaritas!!!")
+print("Hey boys juan arrived he brought the margaritas!!!")
 
-print("this is the fourth branch in the third branch")
+print("This is the fourth branch in the third branch.")
 
-print("i brought music")
+print("I brought music.")
 
-print("lets get the party started")
+print("Let's get the party started.")
 
-print("brought beer to the party")
+print("Brought beer to the party.")
 
-print("secret party in the second branch")
+print("Secret party in the second branch.")
 
-print("only party animals allowed")
+print("Only party animals allowed.")
 
 print("I bought the Mario Kart: Double Dash with 4 controllers!")
