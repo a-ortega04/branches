53330d4f2369cfbfb0922cbe1a92fcaa80f98ac4
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-04-26 17:28:41 -0700

    Update main.py
    added music

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("this is the main branch")
 print("party in the main branch")
 
-print ("this is the secondary branch")
-
 print("hey boys ivan arrived he brought the margaritas!!!")
 
 print("this is the third branch")
+
+print("i brought music")
